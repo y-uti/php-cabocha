9211bc1d40281242bcdb17e31e5f72d7d014cc27
Yuji Uchiyama <y.uchiyama.1015@gmail.com>
2016-10-28 23:19:48 +0900

    Bump version to 0.3.0

--- php_cabocha.h
+++ php_cabocha.h
@@ -4,7 +4,7 @@
 extern zend_module_entry cabocha_module_entry;
 #define phpext_cabocha_ptr &cabocha_module_entry
 
-#define PHP_CABOCHA_VERSION "0.2.0-dev"
+#define PHP_CABOCHA_VERSION "0.3.0"
 
 #ifdef PHP_WIN32
 #   define PHP_CABOCHA_API __declspec(dllexport)
